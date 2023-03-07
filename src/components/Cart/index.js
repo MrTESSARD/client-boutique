9bc643841188e72d03b5e36027dc2ca302ee147e
MrTESSARD <dimitri@tessard.pro>
2023-03-07 15:08:19 +0100

    73. Selector : état dérivé

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -2,26 +2,27 @@ import React from "react";
 import { useSelector } from "react-redux";
 import CartFooter from "./CartFooter";
 import { Table } from "../components";
-import  Row  from "./Row";
-
-
+import Row from "./Row";
+import { selectCartTotal } from "../../lib/redux/selectors";
 
 function Cart({ value }) {
   const items = useSelector((state) => state.items);
+  const total = useSelector(selectCartTotal);
   // debugger;
   console.log(value);
   return (
-    <Table 
-    items={items} 
-    heading="My Shopping Cart" 
-    subheading="items in your cart">
-      
+    <Table
+      items={items}
+      heading="My Shopping Cart"
+      subheading="items in your cart"
+    >
       <tbody>
-        {!items.length &&          <div>No Items in the cart yet </div>}
-        {items.map((item)=>(<Row key={item.id} {...item}/>))}
-
+        {!items.length && <div>No Items in the cart yet </div>}
+        {items.map((item) => (
+          <Row key={item.id} {...item} />
+        ))}
       </tbody>
-      <CartFooter />
+      <CartFooter  />
     </Table>
   );
 }
